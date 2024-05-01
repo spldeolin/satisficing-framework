7473dc6fc38cdd362cf1535f30587b8552e18d28
Deolin <splendid.deolin@gmail.com>
2024-05-02 07:23:45 +0800

    OPTIM: 线程池配置优化

--- satisficing-app/src/main/java/com/spldeolin/satisficing/framework/app/config/ThreadPoolConfig.java
+++ satisficing-app/src/main/java/com/spldeolin/satisficing/framework/app/config/ThreadPoolConfig.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 /**
  * @author Deolin 2024-04-03
@@ -13,22 +14,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ThreadPoolConfig {
 
-    @Value("${thread-pool.core-size}")
+    @Value("${thread-pool.core-size:4}")
     private Integer coreSize;
 
-    @Value("${thread-pool.maximum-size}")
+    @Value("${thread-pool.maximum-size:8}")
     private Integer maximumSize;
 
-    @Value("${thread-pool.queue-capacity}")
+    @Value("${thread-pool.queue-capacity:20}")
     private Integer queueCapacity;
 
-    @Value("${thread-pool.keep-alive-seconds}")
+    @Value("${thread-pool.keep-alive-seconds:60}")
     private Integer keepAliveSeconds;
 
     @Bean("globalThreadPoolExecutor")
     public ThreadPoolExecutor threadPoolExecutor() {
         ThreadPoolExecutor executor = new ThreadPoolExecutor(coreSize, maximumSize, keepAliveSeconds, TimeUnit.SECONDS,
-                new ArrayBlockingQueue<>(queueCapacity), new ThreadPoolExecutor.AbortPolicy());
+                new ArrayBlockingQueue<>(queueCapacity), new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setThreadFactory(new ThreadFactoryBuilder().setNameFormat("global-%d").build());
         return executor;
     }
 
