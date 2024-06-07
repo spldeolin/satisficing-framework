aca806d8a3cdc7e84b694413aa8cdfaaabeb7296
Deolin <splendid.deolin@gmail.com>
2024-06-07 22:06:52 +0800

    ADD: AuthcFilter异常处理对接ExceptionHanler

--- satisficing-security-client/src/main/java/com/spldeolin/satisficing/security/client/filter/AuthcFilter.java
+++ satisficing-security-client/src/main/java/com/spldeolin/satisficing/security/client/filter/AuthcFilter.java
@@ -48,53 +48,57 @@ public class AuthcFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        try {
+            // 获取mvcHandler声明的认证规则
+            HandlerMethod handlerMethod = getHandlerMethod(request);
+            if (handlerMethod == null) {
+                log.warn("No handlerMethod found for request, uri={}", request.getRequestURI());
+                filterChain.doFilter(request, response);
+                return;
+            }
+            List<AuthcRule> authcRules = authcRulesCollector.listAuthcRules(handlerMethod);
 
-        // 获取mvcHandler声明的认证规则
-        HandlerMethod handlerMethod = getHandlerMethod(request);
-        if (handlerMethod == null) {
-            log.warn("No handlerMethod found for request, uri={}", request.getRequestURI());
-            filterChain.doFilter(request, response);
-            return;
-        }
-        List<AuthcRule> authcRules = authcRulesCollector.listAuthcRules(handlerMethod);
+            // 部分系统handler 不解析token，不认证
+            if (handlerMethod.getBeanType() == BasicErrorController.class) {
+                filterChain.doFilter(request, response);
+                return;
+            }
 
-        // 部分系统handler 不解析token，不认证
-        if (handlerMethod.getBeanType() == BasicErrorController.class) {
-            filterChain.doFilter(request, response);
-            return;
-        }
+            // 规则为none 不解析token，不认证
+            if (authcRules.contains(AuthcRule.NONE)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
 
-        // 规则为none 不解析token，不认证
-        if (authcRules.contains(AuthcRule.NONE)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        // 解析token，存入上下文
-        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
-        if (token != null) {
-            RequestResult<IsLoginRespDto> rpcResp = ssoClient.isLogin(new IsLoginReqDto().setToken(token));
-            if (rpcResp.getErrorCode() == null) {
-                if (Boolean.TRUE.equals(rpcResp.getData().getIsLogin())) {
-                    LoginSession.setCurrent(rpcResp.getData().getLoginSession());
+            // 解析token，存入上下文
+            String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+            if (token != null) {
+                RequestResult<IsLoginRespDto> rpcResp = ssoClient.isLogin(new IsLoginReqDto().setToken(token));
+                if (rpcResp.getErrorCode() == null) {
+                    if (Boolean.TRUE.equals(rpcResp.getData().getIsLogin())) {
+                        LoginSession.setCurrent(rpcResp.getData().getLoginSession());
+                    }
+                } else {
+                    log.warn("fail to rpc ssoClient.isLogin");
                 }
             } else {
-                log.warn("fail to rpc ssoClient.isLogin");
+                log.debug("no token found in request");
             }
-        } else {
-            log.debug("no token found in request");
-        }
 
-        // 规则为anonymous 解析token，但不认证
-        if (authcRules.contains(AuthcRule.ANONYMOUS)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
+            // 规则为anonymous 解析token，但不认证
+            if (authcRules.contains(AuthcRule.ANONYMOUS)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
 
-        // 规则是其他情况 解析token，进行认证
-        if (LoginSession.getCurrent() == null) {
-            handlerExceptionResolver.resolveException(request, response, null,
-                    new UnauthcRequestException("用户未登录"));
+            // 规则是其他情况 解析token，进行认证
+            if (LoginSession.getCurrent() == null) {
+                handlerExceptionResolver.resolveException(request, response, null,
+                        new UnauthcRequestException("用户未登录"));
+                return;
+            }
+        } catch (Exception e) {
+            handlerExceptionResolver.resolveException(request, response, null, e);
             return;
         }
 
